f6f959efaffb5881f96c1ef3cf211d79be07064f
liuhe <liuhe@juran.com.cn>
2018-04-27 09:20:30 +0800

    update

--- src/main/java/com/liuhe/practice/DoubleLinkList.java
+++ src/main/java/com/liuhe/practice/DoubleLinkList.java
@@ -5,61 +5,69 @@ package main.java.com.liuhe.practice;
  * （1）头插入：要对链表进行判断，如果为空则设置尾节点为新添加的节点，如果不为空，还要设置头节点的一个前节点为新节点
  * （2）尾插入：如果链表为空，则直接设置头节点为新添加的节点，否则设置尾节点的后一个节点为新添加的节点。同时设置新添加的节点的前一个节点为尾节点
  * (3) 头删除 :判断节点是否有下个节点，如果没有则设置节点为null，并且删除下个节点指向前节点的指针
+ * https://blog.csdn.net/a19881029/article/details/22695289
  *
  * @author liuhe
  * @date 2018/4/26
  */
 public class DoubleLinkList {
 
-    DoubleLinkList pre = null;
-    DoubleLinkList tail = null;
-    int data;
-
-    public DoubleLinkList(DoubleLinkList pre, DoubleLinkList tail, int data) {
-        this.pre = pre;
-        this.tail = tail;
-        this.data = data;
-    }
-
-    public DoubleLinkList(DoubleLinkList pre, DoubleLinkList tail) {
-        this.pre = pre;
-        this.tail = tail;
-    }
-
-    public DoubleLinkList(int data) {
-        this.data = data;
-    }
+    Node header = null;
+    Node tail = null;
 
     /**
      * 头插入
      *
      * @param data
      */
-    void appendFirst(int data) {
-        DoubleLinkList newNode = new DoubleLinkList(data);
-        // 如果头结点为null，那使用当前节点为双链的尾节点
-        if (null == this.pre) {
-            this.tail = newNode;
+    void add(int data) {
+        Node node = new Node(data);
+        /**
+         * 如果头结点等于null 说明还没有数据 头结点和尾节点都是当前节点
+         */
+        if (null == header) {
+            header = node;
+            tail = node;
         } else {
-            this.pre.pre = newNode;
-            //把first节点往下移动
-            newNode.tail = pre;
+            /**
+             * 默认从尾部添加
+             */
+            tail.next = node;
+            node.pre = tail;
+            tail = node;
         }
-        pre = newNode;
     }
 
+    boolean delete(int data) {
+        Node temp = find(data);
+
+    }
+
+    public Node find(int data) {
+        Node cur = header;
+        while (null != cur) {
+            if (cur.data == data) {
+                return cur;
+            }
+            cur=cur.next;
+        }
+        return null;
+    }
+
+
     /**
-     * 尾插入
-     *
-     * @param data
+     * 节点类
      */
-    void appendLast(int data) {
-        DoubleLinkList newNode = new DoubleLinkList(data);
-        if (null == pre) {
-            pre = newNode;
-        } else {
-            newNode.pre = tail.pre;
-            tail = newNode;
+    private static class Node {
+        // 上一个
+        Node pre;
+        // 数据
+        Object data;
+        // 下一个
+        Node next;
+
+        public Node(Object data) {
+            this.data = data;
         }
     }
 }
